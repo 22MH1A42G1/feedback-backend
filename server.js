387e6647a4112e0c215f26186adc596b193e6a48
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-08-05 21:28:05 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
@@ -10,11 +12,12 @@ app.use(express.json());
 
 // MySQL connection setup
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Aditya@1710',
-    database: 'feedbackDB'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
+
 
 db.connect(err => {
     if (err) throw err;
